0bcf4c08a9e26641965853b06d6c065490a5ec86
trey <treyoehmler@gmail.com>
2019-03-17 14:31:14 -0400

    copy changes

--- resources/js/views/Cover.vue
+++ resources/js/views/Cover.vue
@@ -26,11 +26,10 @@
                   <div id="about"></div>
               </div>
               <div class="step" data-step-no="2">
-                  <p class="h5 fw-500 mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla egestas nibh vitae purus tincidunt malesuada. Nunc id odio nec diam blandit porttitor.</p>
-                  <p class="fw-300">Ut eleifend eros in augue bibendum, sed tincidunt ex finibus. Phasellus non sapien posuere tortor tempor aliquet. Phasellus at ante sed nisl luctus vestibulum. Pellentesque quis ante pulvinar, sodales mi rhoncus, ultrices ex. Sed ligula lacus, iaculis eu mollis ut, consequat eget nisl. Morbi ornare nulla ut fermentum facilisis.</p> 
-                  <p class="fw-300 mb-5">Morbi massa nunc, luctus et dui eu, aliquet posuere sapien. Suspendisse eu ultricies ex, sed blandit enim. Vivamus imperdiet pulvinar dictum. Suspendisse tempor pretium aptent taciti sociosqu ad litora torquent.</p>
+                  <p class="h5 fw-500 mb-3">My name is Trey. I'm 20 years old and a web designer, developer and college student. I’m currently studying computer science and math at Middlebury College.</p>
+                  <p class="fw-300">At eleven years old I began teaching myself to program. Starting with simple websites, I worked my way up to building full-stack web apps, iPhone apps, and developing for motion sensing technology. Nowadays, I have a wide range of development-related interests, though I primarily focus my efforts on creating immersive, responsive and memorable websites.</p> 
                   <p class="h4 fw-500">Technical Experience</p>
-                  <p class="fw-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in risus eu justo dapibus vestibulum. Nam nibh nisi, luctus et venenatis quis, commodo sed odio. Nam vestibulum nisi non enim sollicitudin dictum. Donec blandit blandit risus at fringilla. Donec varius massa ac auctor hendrerit. Nulla dui ex, rutrum tempor nisi convallis, pharetra egestas nulla. Cras scelerisque urna in egestas pretium.</p>
+                  <p class="fw-300">I have a passion for problem solving and figuring things out. I try to learn as many new skills and technologies as possible so that I can deliver work that’s in touch with current digital design trends while performing well.  I’ve been around a computer my entire life, and been programming them for the last eight years. Recently, I’ve turned my focus  towards full-stack web development. Below I’ve listed the tools and languages I feel most comfortable using.</p>
                   <p>
                       <span class="badge badge-pill badge-dark">HTML / CSS</span>
                       <span class="badge badge-pill badge-dark">Sass</span>
@@ -52,24 +51,21 @@
               <!-- TODO: ADD TECHNICAL SKILLS -->
               <div class="step" data-step-no="3">
                   <p class="h2 fw-500">Recent Projects</p>
-                  <p class="fw-300">Mauris accumsan, quam sed commodo mattis, lorem eros mattis lorem, ac fermentum turpis orci eu tortor. Vestibulum tincidunt hendrerit accumsan. Duis egestas tempus diam, ac molestie turpis venenatis quis.</p>
-
+                  <p class="fw-300">I can handle projects from scratch or projects for established brands with preexisting visuals and aesthetics. Below is a collection of some of my web development work and some of the projects I'm currently involved in. </p>
               </div>
               <div class="step" data-step-no="4">
                   <a class="project-link" href="https://domcbd.co" target="__blank">
                       <img src="/domcbd.png" class="img-fluid mb-3">
                   </a>
                   <a href="https://domcbd.co" class="h5 fw-500 project-link" target="__blank">domcbd.co</a>
-                  <p class="fw-300">Proin varius dapibus neque venenatis iaculis. Donec pretium, enim sit amet congue iaculis, lectus tortor ornare nisl, ac lobortis lectus elit sed eros. Proin sed tempus arcu. Donec at leo a nibh laoreet pulvinar.</p>
-                  <p class="fw-300">Ut bibendum quis dolor eget fringilla. Integer semper sapien a tellus dictum auctor. Phasellus a tempus magna. Maecenas dolor nunc, suscipit vitae pharetra et, aliquet sit amet turpis.</p>
+                  <p class="fw-300">An online storefront for DomCBD. Includes a blog with a custom content management system built from the ground up.</p>
               </div>
               <div class="step" data-step-no="5">
                   <a class="project-link" href="https://dompen.co" target="__blank">
                       <img src="/dompen.png" class="img-fluid mb-3">
                   </a>
                   <a href="https://dompen.co" class="h5 fw-500 project-link" target="__blank">dompen.co</a>
-                  <p class="fw-300">Donec pellentesque lacus in dolor placerat, eget fringilla magna iaculis. Nunc mollis bibendum bibendum. Mauris sed lorem faucibus, scelerisque velit a, malesuada sem.</p>
-                  <p class="fw-300">nteger ut mauris efficitur, ultrices nisl vel, tincidunt mauris. Morbi fringilla sodales blandit. Nulla sollicitudin aliquet lectus, vel auctor sem. Morbi leo metus, pellentesque sit amet efficitur sit amet, vestibulum sit.</p>
+                  <p class="fw-300">A informational and branding site for DomPen. Includes a blog, store locatator and dynamic, high-quality graphics.</p>
               </div>
               <div class="step" data-step-no="6">
                   <a class="project-link" href="https://onelifemh.org" target="__blank">
@@ -78,12 +74,11 @@
               </div>
               <div class="step" data-step-no="7">
                   <a href="https://onelifemh.org" target="__blank" class="project-link h5 fw-500">onelifemh.org</a>
-                  <p class="fw-300">Praesent augue nisi, pretium ut erat a, porta feugiat nulla. Nulla vitae justo mauris. Phasellus varius nunc purus, non imperdiet felis pellentesque a. Aenean pharetra ligula sit amet turpis scelerisque porta. Quisque accumsan gravida eros a egestas. Curabitur dolor dui, sollicitudin in ante in, dignissim congue tellus.</p>
-                  <p class="fw-300">nteger ut mauris efficitur, ultrices nisl vel, tincidunt mauris. Morbi fringilla sodales blandit. Nulla sollicitudin aliquet lectus, vel auctor sem. Morbi leo metus, pellentesque sit amet efficitur sit amet, vestibulum sit.</p>
+                  <p class="fw-300">A promotional website for OneLife, a fundraising organization for pediatric brain cancer. Includes newsletter integration and Stripe payment processing.</p>
               </div>
               <div id="contact" class="step" data-step-no="8">
                   <p class="h2 fw-500 mb-3">Contact</p>
-                  <p class="fw-300">Integer pulvinar eu enim at mollis. Vivamus in dapibus leo. Ut sed risus est. Cras nec mauris euismod ligula laoreet dictum quis vitae magna. Ut viverra elementum consequat. Sed convallis eu justo vel ornare. Etiam vel sodales neque. Cras porttitor nisi in felis luctus, ac consectetur orci.</p>
+                  <p class="fw-300">Interested in starting a project with me? Feel free to reach out! I work with my clients to determine compensation well below the market average, while still creating websites that will set you, your brand, or your organization apart.</p>
                   <a href="mailto:treyoehmler@gmail.com"><i class="far fa-envelope contact-icon"></i><span class="small">treyoehmler@gmail.com</span></a>
                   <a href="tel:203-722-1497"><i class="fas fa-phone contact-icon"></i><span class="small">(203) 722-1497</span></a>
                   <a href="https://github.com/toehmler" target="__blank"><i class="fab fa-github contact-icon"></i><span class="small">toehmler</span></a>
