5512d8a257b07917cd24e6b1b80a5e6fbbe8cb8e
toehlmer <treyoehmler@gmail.com>
2021-02-26 15:27:10 -0800

    update og:image tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,15 +4,15 @@
 		  <meta charset="utf-8">
 		
 		  <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
-          <meta id="csrf-token" name="csrf-token" content="{{ csrf_token() }}">
+      <meta id="csrf-token" name="csrf-token" content="{{ csrf_token() }}">
 
-          <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-          <link rel="stylesheet" href="/css/animations.css">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-          <link href="/css/hamburgers.css" rel="stylesheet">
-          <link rel="icon" type="image/png" href="favicon.png">
-        <meta name="image" property="og:image" content="https://treyoehmler.com/unet_tile.jpg" />
+      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+      <link rel="stylesheet" href="/css/animations.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+      <link href="/css/hamburgers.css" rel="stylesheet">
+      <link rel="icon" type="image/png" href="favicon.png">
+      <meta name="image" property="og:image" content="https://treyoehmler.com/tumor_tile.jpg" />
      </head>
      <body>
          <div id="app"></div>
