778be358662bf03286de035ef10d102cdf36f751
toehlmer <treyoehmler@gmail.com>
2021-02-26 15:25:11 -0800

    og:image

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,6 +12,7 @@
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
           <link href="/css/hamburgers.css" rel="stylesheet">
           <link rel="icon" type="image/png" href="favicon.png">
+        <meta name="image" property="og:image" content="https://treyoehmler.com/unet_tile.jpg" />
      </head>
      <body>
          <div id="app"></div>
