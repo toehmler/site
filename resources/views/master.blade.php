7759e654ba7379799b73bb4cc8c2634874026d5d
toehlmer <treyoehmler@gmail.com>
2021-02-26 15:28:37 -0800

    update og:image tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,7 @@
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
       <link href="/css/hamburgers.css" rel="stylesheet">
       <link rel="icon" type="image/png" href="favicon.png">
-      <meta name="image" property="og:image" content="https://treyoehmler.com/tumor_tile.jpg" />
+      <meta name="image" property="og:image" content="https://treyoehmler.com/tumor_tile_li.jpg" />
      </head>
      <body>
          <div id="app"></div>
