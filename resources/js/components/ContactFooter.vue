547f6cfc228fe3d48f02cef47b7d7293bde8b51c
toehlmer <treyoehmler@gmail.com>
2021-03-01 14:52:30 -0800

    small fixes

--- resources/js/components/ContactFooter.vue
+++ resources/js/components/ContactFooter.vue
@@ -4,13 +4,15 @@
       <div class="row py-5 justify-content-center align-items-center">
         <div class="order-2 order-md-1 col-xl-5 col-sm-10 col-md-8 col-lg-7 text-center text-md-left">
           <p class="h5 serif mb-5">I’m always up for new opportunities and projects. Feel free to say hello via <span class="text-light">treyoehmler@gmail.com</span></p>
-          <a href="/">LinkedIn</a>
-          <a href="/" class="mx-5">GitHub</a>
-          <a href="/">Phone</a>
+          <a href="https://www.linkedin.com/in/toehmler/" target="_blank">LinkedIn</a>
+          <a href="https://github.com/toehmler" class="mx-5" target="_blank">GitHub</a>
+          <a href="tel:+12037221497">Phone</a>
         </div>
         <div class="order-1 order-md-2 col-9 col-sm-5 col-md-4 col-lg-3 offset-xl-1 text-center mb-5 mb-md-0">
           <img src="/resume_icon.jpg" class="img-fluid mb-4">
-          <button href="/" class="light-button">DOWNLOAD CV</button>
+          <a href="trey_oehmler_resume.pdf" target="_blank">
+            <button href="./trey_oehmler_resume.pdf" class="light-button" target="_blank">VIEW CV</button>
+          </a>
         </div>
       </div>
     </div>
